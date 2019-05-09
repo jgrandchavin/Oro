06590246bb859429fcfd32d9baa614e6e723b156
Julien Grand-Chavin <jgrandchavin@MacBook-Pro-de-Julien.local>
2019-05-09 14:12:49 +0200

    Push

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
-# oro
+# Oro APP
 
-A new Flutter project.
+## Author
+Julien Grand-Chavin, 23 years old, from France / Switzerland.
 
-## Getting Started
+## Platform
+Oro works with both iOS and Android platform. (Work well on iPhone X and Honor X8).
 
-This project is a starting point for a Flutter application.
+## Description 
+My app localizes you and gives you your localization's AQI (Air quality Index) and you get the information with a shake. 
 
-A few resources to get you started if this is your first Flutter project:
+Here is the different categories of air quality:
+- [0:30] : Excellent air quality
+- ]30 : 50] : Good air quality
+- ]50 : 100] : Moderate air quality
+- ]100 : 500] : Low air quality
 
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
+Each category has one color and one Flare animation (different for each). Don't forget to watch the animation :D  
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+The app gives you the weather with the temperature and an icon.
+
+PS: I let you some screenshots in the folder to get a view of each case.
+
+## API
+I use two API for the project :
+- https://nominatim.openstreetmap.org/reverse : to get exact location name (Free to use)
+- https://api.airvisual.com/v2/nearest_city? : to get the weather and the AQI (I have a free community key limited to 10 000 call per month)
+
+
+## Thanks
+I hope you enjoy my app and congratulations for the Flutter Create, it was fun to do ! 
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_provider/bloc_provider.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:location/location.dart';
 import 'package:sensors/sensors.dart';
@@ -11,110 +12,183 @@ import 'package:rxdart/rxdart.dart';
 const grey = Color(0xff5E5E5E);
 const lightGrey = Color(0xffE3E3E3);
 const purple = Color(0xff6E46B8);
-const pink = Color(0xffC97CBE);
-const green= Color(0xff86D9B0);
+const green = Color(0xff86D9B0);
 const orange = Color(0xffC9A57C);
+const blue = Color(0xff7CC3C9);
 const red = Color(0xffC97C7C);
+space(double h, double w) => SizedBox(height: h, width: w);
+text(String t, double s, Color c, FontWeight f) =>
+    Text(t, style: TextStyle(color: c, fontWeight: f, fontSize: s));
+svg(String a, double h, double w, Color c) =>
+    SvgPicture.asset(a, color: c, height: h, width: w);
+edgeInsests(double t, double b, double r, double l) =>
+    EdgeInsets.only(left: l, right: r, top: t, bottom: b);
+headerCont(c) => Container(
+    padding: edgeInsests(5, 5, 10, 10),
+    decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+              color: lightGrey.withOpacity(0.4),
+              blurRadius: 1,
+              offset: Offset(0, 2))
+        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(7),
+        ),
+        color: Colors.white),
+    child: c);
 
 class AQ {
-  var city, aqi, cat, reco, color, asset;
-  AQ.fJ(j): aqi = j['data']['indexes']['baqi']['aqi'], cat = j['data']['indexes']['baqi']['category'], reco = j['data']['health_recommendations']['general_population'];
+  var city, aqi, category, catNumb, reco, color, asset, tmp, icon;
+  AQ.fJ(j)
+      : aqi = j['data']['current']['pollution']['aqius'],
+        tmp = j['data']['current']['weather']['tp'],
+        icon = 'assets/${j['data']['current']['weather']['ic']}.svg';
 }
 
 main() => runApp(MaterialApp(
-  theme: ThemeData(fontFamily: 'Averta', scaffoldBackgroundColor: purple),
-  home: BlocProvider<NavBloc>(creator: (_c, _b) => NavBloc(),child: Scaffold(resizeToAvoidBottomPadding: false,body: CurrentView()))
-));
+    theme: ThemeData(fontFamily: 'Averta', scaffoldBackgroundColor: purple),
+    home: BlocProvider<NavBloc>(
+        creator: (c, b) => NavBloc(), child: Scaffold(body: CurrentView()))));
 
 class NavBloc implements Bloc {
-  ReplaySubject<Widget> _viewController = ReplaySubject<Widget>();
-  Sink<Widget> get _inView => _viewController.sink;
-  Stream<Widget> get outView => _viewController.stream;
+  BehaviorSubject<Widget> _viewCtrl = BehaviorSubject<Widget>();
+  Sink<Widget> get _inView => _viewCtrl.sink;
+  Stream<Widget> get outView => _viewCtrl.stream;
   NavBloc() {
-    var x = 0;
-    accelerometerEvents.listen((e) {
-      if (e.x > x + 30) _inView.add(LoadView());
-    });
+    accelerometerEvents
+        .where((e) => e.x > 20 && _viewCtrl.value.toString() != 'LoadView')
+        .listen((_) => _inView.add(LoadView()));
   }
   showResV(aq) => _inView.add(ResultView(aq));
-  dispose() => _viewController.close();
+  dispose() => _viewCtrl.close();
 }
 
 class CurrentView extends StatelessWidget {
   build(c) {
     var nB = BlocProvider.of<NavBloc>(c);
-    return StreamBuilder(stream: nB.outView, initialData: HomeView(),builder: (c, s) => s.data);
+    return StreamBuilder(
+        stream: nB.outView, initialData: homeView(), builder: (c, s) => s.data);
   }
 }
 
 class LoadView extends StatelessWidget {
   build(c) {
     var nB = BlocProvider.of<NavBloc>(c);
-    return FutureBuilder(future: getAQ(), builder: (c, s) {
-			if (s.hasData) nB.showResV(s.data);
-			return SvgPicture.asset('assets/load.svg',	fit: BoxFit.fitHeight,);
-		});
+    return FutureBuilder(
+        future: getAQ(),
+        builder: (c, s) {
+          if (s.hasData) nB.showResV(s.data);
+          return Stack(
+              fit: StackFit.expand,
+              children: [SvgPicture.asset('assets/load.svg')]);
+        });
   }
 
   getAQ() async {
-    var loc = await Location().getLocation();
-    var r = await http.get('https://api.breezometer.com/air-quality/v2/current-conditions?lat=${loc.latitude}&lon=${loc.longitude}&key=adf7c0d3e70441959f06a59d772190b0&features=breezometer_aqi,health_recommendations');
-    var r2 = await http.get('http://nominatim.openstreetmap.org/reverse?format=json&lat=${loc.latitude}&lon=${loc.longitude}');
-    AQ aq = AQ.fJ(c.jsonDecode(r.body));
-    aq.color = aq.aqi >= 60 ? green : (aq.aqi < 40 ? red : orange);
-    aq.asset = aq.aqi >= 60 ? 'assets/1.flr' : (aq.aqi < 40 ? 'assets/3.flr' : 'assets/2.flr');
-    var a = c.jsonDecode(r2.body)['address'];
-    aq.city = a['city'] != null ? a['city'] : (a['village'] != null ? a['village'] : 'N/A');
-  
+    var loc = await Location().getLocation(),
+        json =
+            await c.jsonDecode(await rootBundle.loadString('assets/text.json')),
+        r = await http.get(
+            'http://api.airvisual.com/v2/nearest_city?lat=${loc.latitude}&lon=${loc.longitude}&key=qZDARMBt8RLaHyiMd'),
+        r2 = await http.get(
+            'http://nominatim.openstreetmap.org/reverse?format=json&lat=${loc.latitude}&lon=${loc.longitude}&zoom=10'),
+        aq = AQ.fJ(c.jsonDecode(r.body)),
+        a = c.jsonDecode(r2.body)['address']['city'];
+    aq.catNumb =
+        aq.aqi < 31 ? '1' : (aq.aqi < 51 ? '2' : (aq.aqi < 101 ? '3' : '4'));
+    aq.color = aq.aqi < 31
+        ? blue
+        : (aq.aqi < 51 ? green : (aq.aqi < 101 ? orange : red));
+    aq.reco = json['reco'][aq.catNumb];
+    aq.asset = json['assets'][aq.catNumb];
+    aq.city = a != null ? a : 'N/A';
+    aq.category = json['quality'][aq.catNumb];
     return aq;
   }
 }
 
-class HomeView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(children: <Widget>[
-      SvgPicture.asset('assets/home.svg',fit: BoxFit.fitHeight,),
-      FlareActor("assets/shake.flr",animation: "shake",)
-  ]);
+homeView() => Stack(fit: StackFit.expand, children: [
+      SvgPicture.asset('assets/home.svg'),
+      FlareActor('assets/shake.flr', animation: 'shake')
+    ]);
+
+class ResultView extends StatelessWidget {
+  final aq;
+  ResultView(this.aq);
+  build(c) {
+    var dec = BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.horizontal(right: Radius.circular(10))),
+        media = MediaQuery.of(c).size;
+    return Scaffold(
+        backgroundColor: aq.color,
+        body: Stack(fit: StackFit.expand, children: [
+          Positioned(
+              bottom: 0,
+              child: Container(
+                  height: media.height * 0.4,
+                  width: media.width,
+                  child: FlareActor(aq.asset,
+                      fit: BoxFit.fitWidth, animation: 'move'))),
+          Positioned(
+              bottom: 0,
+              left: 50,
+              right: 50,
+              child: Container(
+                  height: 100,
+                  child: FlareActor('assets/shake2.flr', animation: 'shake'))),
+          SafeArea(
+              child: Column(children: [
+            Container(
+                margin: edgeInsests(30, 20, 75, 0),
+                padding: edgeInsests(20, 20, 16, 16),
+                decoration: dec,
+                child: Column(children: [
+                  Row(children: [
+                    svg('assets/place.svg', 26, 20, grey),
+                    space(0, 15),
+                    text(aq.city, 26, grey, FontWeight.w600)
+                  ]),
+                  space(15, 0),
+                  Row(children: [
+                    headerCont(text('${aq.tmp}°C', 18, grey, FontWeight.w600)),
+                    space(0, 10),
+                    headerCont(svg(aq.icon, 21, 21, grey))
+                  ])
+                ])),
+            Container(
+                padding: edgeInsests(50, 50, 32, 26),
+                margin: edgeInsests(0, 0, 75, 0),
+                decoration: dec,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            text('${aq.aqi}', 30, aq.color, FontWeight.w800),
+                            space(0, 10),
+                            text('Air Quality Index', 19, aq.color,
+                                FontWeight.w200)
+                          ]),
+                      space(10, 0),
+                      text(aq.category, 20, grey, FontWeight.w600),
+                      space(10, 0),
+                      Row(children: [
+                        star(aq.color, aq.aqi < 101),
+                        star(aq.color, aq.aqi < 51),
+                        star(aq.color, aq.aqi < 31)
+                      ]),
+                      space(40, 0),
+                      text(aq.reco, 15, grey, FontWeight.w200)
+                    ]))
+          ]))
+        ]));
   }
 }
 
-class ResultView extends StatelessWidget {
-  final AQ aq;
-  ResultView(this.aq);
-  build(BuildContext context) {
-    var dec = BoxDecoration(color: Colors.white, borderRadius: BorderRadius.horizontal(right: Radius.circular(10)));
-    return Scaffold(
-      backgroundColor: aq.color,
-      body: Stack(children: [
-        FlareActor(aq.asset, fit:BoxFit.fitHeight, animation: 'move'),
-        Positioned( bottom: 30, left: 50,right: 50,child: Container( height: 100,child: FlareActor("assets/shake2.flr", animation: "shake",))),
-        SafeArea(
-          child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Container(
-            margin: EdgeInsets.only(top: 50, bottom: 20, right: 75),
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-            decoration: dec,
-            child: Row(children: [
-              SvgPicture.asset('assets/p.svg', height: 30, color: grey,),
-              SizedBox( width: 15,),
-              Text(aq.city,style: TextStyle(fontSize: 26,fontWeight: FontWeight.w600, color: grey))])),
-          Container(
-            padding: EdgeInsets.only(top: 50,bottom: 50, left: 16, right: 32),
-            margin: EdgeInsets.only(right: 75),
-            decoration: dec,child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text('${aq.aqi}/100',style: TextStyle(color: aq.color, fontSize: 26, fontWeight: FontWeight.w800)),
-              SizedBox(height: 10),
-              Text( aq.cat, style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),),
-              SizedBox( height: 10),
-              Row(children: <Widget>[star(aq.color, true), star(aq.color, aq.aqi > 25), star(aq.color, aq.aqi > 50), star(aq.color, aq.aqi > 75)],),
-              SizedBox( height: 40),
-              Text(aq.reco, style:TextStyle(color: grey, fontWeight: FontWeight.w200, fontSize: 15)),
-      ]))]))]));
-    }
-}
-
-
-Widget star(color, isColored) => Container(padding:EdgeInsets.only(right: 10), child:SvgPicture.asset('assets/star.svg',color: isColored ? color:lightGrey));
+star(color, isColored) => Container(
+    padding: edgeInsests(0, 0, 16, 0),
+    child: SvgPicture.asset('assets/star.svg',
+        color: isColored ? color : lightGrey));
